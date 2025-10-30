41fc763b895d1ffb25c4c1cd5b7aa9d0a0d4e3cc
Prince Robin <princerobinraj@gmail.com>
2025-10-30 14:26:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>Hello World 🌍</h1>
+      <p>Welcome to your first React app!</p>
+    </div>
+  );
+}
+
+export default App;
