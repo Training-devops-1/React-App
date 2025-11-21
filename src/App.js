0eac1212cd06184cf66ef3673767a7738072272d
Prince Robin <princerobinraj@gmail.com>
2025-11-21 17:23:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "20%" }}>
       <h1>Hello World 🌍</h1>
-      <p>Welcome first to React app!</p>
+      <p>Welcome to React app!</p>
     </div>
   );
 }
